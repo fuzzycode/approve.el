8ac65b0fd19853755cad0e91d94f5c62ac8652ac
Björn Larsson <develop@bjornlarsson.net>
2025-12-29 17:35:58 +0100

    Add commits file

---
+++ lisp/approve-ui-commits.el
@@ -0,0 +1,20 @@
+;;; approve-ui-commits.el --- Commit UI components for Approve -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Björn Larsson
+
+;; Author: Björn Larsson
+;; URL: https://github.com/fuzzycode/approve
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "28.1"))
+
+;; SPDX-License-Identifier: MIT
+
+;;; Commentary:
+
+;; This file provides UI components for displaying commit information
+;; in Approve pull request buffers.
+
+;;; Code:
+
+(provide 'approve-ui-commits)
+;;; approve-ui-commits.el ends here
