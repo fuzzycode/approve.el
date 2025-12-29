58acb2dc8c952cdad2433b577a083602b7ac6d3e
Björn Larsson <develop@bjornlarsson.net>
2025-12-29 19:51:16 +0100

    Fix the formatting

--- lisp/approve-ui-commits.el
+++ lisp/approve-ui-commits.el
@@ -1,12 +1,30 @@
-;;; approve-ui-commits.el --- Commit UI components for Approve -*- lexical-binding: t; -*-
+;;; approve-ui-commits.el --- Commit UI components for Approve  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2025 Björn Larsson
 
 ;; Author: Björn Larsson
-;; URL: https://github.com/fuzzycode/approve
-;; Version: 0.1.0
+;; Maintainer: Björn Larsson
 
-;; SPDX-License-Identifier: MIT
+;; This file is not part of GNU Emacs.
+
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ;;; Commentary:
 
